0dd3fad8dd466c0e69993e864fb53b2a4b1dba67
Alberto <alberto@hevelop.com>
2021-12-14 10:34:56 +0100

    Update go_installer.sh

--- server/go_installer.sh
+++ server/go_installer.sh
@@ -1,8 +1,23 @@
-export GOLANG=go1.17.5.linux-armv6l.tar.gz
-wget https://go.dev/dl/$GOLANG
-sudo tar -C /usr/local -xzf $GOLANG
-rm $GOLANG
-unset GOLANG
-echo PATH="$PATH:/usr/local/go/bin" >>~/.profile
-echo GOPATH="$HOME/go" >>~/.profile
-cd ~ && source ~/.profile
+#!/bin/bash
+
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+YELLOW=$'\e[1;33m'
+BLUE=$'\e[1;34m'
+ENDCOLOR=$'\e[0m'
+
+check_installation () {
+    if ! [ -x "$(command -v $1)" ];
+    then
+        printf "${RED}✗ $1 not installed${ENDCOLOR}\n"
+        exit 1
+    else
+        printf "${GREEN}✔ $1 installed${ENDCOLOR}\n"
+    fi
+}
+
+printf "\n${YELLOW}Install GOLANG${ENDCOLOR}\n"
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install -y golang-go
+check_installation go
