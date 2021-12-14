8b3eaf85203c01c9fb94caf0818313ef6155f1a0
Alberto <alberto@hevelop.com>
2021-12-14 10:50:16 +0100

    Update go_installer.sh

--- server/go_installer.sh
+++ server/go_installer.sh
@@ -4,8 +4,6 @@ export GOLANG=go1.17.5.linux-armv6l.tar.gz
 wget https://dl.google.com/go/$GOLANG
 sudo mv go /usr/local
 sudo sudo tar -xvf $GOLANG
-export GOROOT=/usr/local/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 rm $GOLANG
 unset GOLANG
 echo GOROOT="/usr/local/go" >>~/.profile
